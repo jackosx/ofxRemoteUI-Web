8dc32f086935c4b0ed9b9369bc1adc5662b79fcd
Jack O'Shea <JacksBook@Jacks-MacBook-Pro.local>
2017-07-31 17:29:29 -0400

    Basic param updates working (float and probably ints), now need presets and other types

--- js/remote-ui.js
+++ js/remote-ui.js
@@ -1,166 +1,254 @@
+///////////////////////////////////////////////////
+//                     DOM                       //
+///////////////////////////////////////////////////
+
+var form = document.getElementById("host-form");
+var hostField = document.getElementById("host-field");
+var statusLabel = document.getElementById("status-label");
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    document.activeElement.blur();
+    var input = hostField.value;
+    setupSocket(input);
+});
+
+function setState(newState) {
+    state = newState;
+    statusLabel.innerHTML = stateMap[state];
+    if (state == 2)
+        createGUI();
+    if (state > 2) {
+        destroyGUI();
+    }
+}
+
 
 ///////////////////////////////////////////////////
 //                  WebSocket                    //
 ///////////////////////////////////////////////////
 
-var socket = new WebSocket("ws://localhost:1234");
+var socket;
+var host;
+var state;
+var stateMap = {
+    0 : "Not Connected.",
+    1 : "Connecting.",
+    2 : "Connected.",
+    3 : "Error Connecting.",
+    4 : "Socket Closed."
+};
+setState(0);
 
-socket.addEventListener('open', function(event) {
-  console.log("Connected to server.");
-});
+function setupSocket(tryhost){
 
-socket.addEventListener('message', function(event) {
-  console.log('Received Message:', event.data);
-  var osc = JSON.parse(event.data);
-  gotMsg[getOscAddr(osc)];
-})
+    setState(1);
 
+    host = tryhost;
+    socket = new WebSocket("ws://" + host);
+
+    socket.onopen = function(event) {
+        console.log("Connected to server.");
+        setState(2);
+        sendOSC("HELO");
+    };
+
+    socket.onmessage = function(event) {
+        console.log('Received Message:', event.data);
+        var osc = JSON.parse(event.data);
+        console.log(osc);
+        var msgAction = getOscAddr(osc);
+        var msgFnc = msgcFuncs[msgAction];
+        msgFnc(osc);
+    };
+
+    socket.onclose = function(event) {
+        console.log("Socket Closed.", event);
+        setState(4);
+    };
+
+    socket.onerror = function(event) {
+        console.log("Socket Error.");
+        setState(3);
+    };
+}
+
+
+///////////////////////////////////////////////////
+//                   Dat.GUI                     //
+///////////////////////////////////////////////////
+var paramVals  = {}; // need to store these
+var paramMetas = {}; // separately for dat.GUI
+var groups = [];
+
+var guiContainer = document.getElementById('controls');
+var placeholderControls = document.getElementById('controls-placeholder');
+var gui;
+
+function createGUI() {
+   placeholderControls.style.display = 'none';
+   gui = new dat.GUI({ autoPlace: false, width: guiContainer.offsetWidth });
+   window.addEventListener("resize", function() {
+       gui.width = guiContainer.offsetWidth;
+   })
+   guiContainer.appendChild(gui.domElement);
+}
+
+
+
+function destroyGUI(){
+    if (gui) {
+        gui.destroy();
+        guiContainer.removeChild(gui.domElement);
+    }
+    paramVals = {};
+    paramMetas = {};
+    groups = [];
+    placeholderControls.style.display = 'block';
+}
 
 
 ///////////////////////////////////////////////////
 //                Quasi-Osc Things               //
 ///////////////////////////////////////////////////
 
-var paramVals  = {}; // need to store these
-var paramMetas = {}; // separately for dat.GUI
-var groups = [];
-
 function createOsc(addr, args) {
-  return {
-            "addr": "/" + addr,
-            "args": args
-         };
+    return {
+        "addr": "/" + addr,
+        "args": args
+    };
+}
+
+function sendOSC(addr, args) {
+    socket.send(JSON.stringify(createOsc(addr, args)));
 }
 
 function getOscAddr(osc) {
-  return osc.addr.substr(1,4);
+    return osc.addr.substr(1,4);
 }
 
 function getHeaderPieces(osc) {
-  return osc.addr.split(' ');
+    return osc.addr.split(' ');
 }
 
-function setParam(data) {}
+// Update the values here in JS
+function setLocalParamViaOsc(osc, type, name) {
 
-function setParamViaOsc(osc, type, name) {
+    if (typeof type === 'undefined') type = getHeaderPieces(osc)[1];
+    if (typeof name === 'undefined') name = getHeaderPieces(osc)[2];
 
-  if (typeof type === 'undefined') type = getHeaderPieces(osc)[1];
-  if (typeof name === 'undefined') name = getHeaderPieces(osc)[2];
+    var args = osc.args;
+    var paramVal = args[0];
+    var paramInfo = { "type" : type, "osc" : osc  };
+    var guiRef = (groups.length > 0) ? groups[0] : gui;
+    var control; // used to listen for value changes
+    var isNewParam = !(paramVals.hasOwnProperty(name));
 
-  var args = osc.args;
-  var paramVal = 1;
-  var paramInfo = {};
+    paramMetas[name] = paramInfo;
 
-  if (type == "FLT") {
+    if (type == "FLT") {
+        paramVals[name] = parseFloat(paramVal);
+        paramInfo.min = parseFloat(args[1]);
+        paramInfo.max = parseFloat(args[2]);
+        if (isNewParam)
+            control = guiRef.add(paramVals, name, paramInfo.min, paramInfo.max).listen();
+    }
+    else if (type == "INT") {
+        paramVals[name] = parseInt(paramVal);
+        paramInfo.min = parseInt(args[1]);
+        paramInfo.max = parseInt(args[2]);
+        if (isNewParam)
+            control = guiRef.add(paramVal, name, paramInfo.min, paramInfo.max).step(1);
+    }
+    else if (type == "BOL") {
+        paramVals[name] = parseBool(paramVal);
+        guiRef.add
+    }
+    else if (type == "STR") {
 
-  }
-  else if (type == "INT") {
+    }
+    else if (type == "ENU") {
 
-  }
-  else if (type == "BOL") {
+    }
+    else if (type == "COL") {
 
-  }
-  else if (type == "STR") {
+    }
 
-  }
-  else if (type == "ENU") {
-
-  }
-  else if (type == "COL") {
-
-  }
-
-
-  if (!(paramVals.hasOwnProperty(name))) {
-    paramVals[name] = paramVal;
-    var guiT = (groups.len > 0) ? groups[0] : gui;
-    guiT.add(paramVals, name);
-  }
-  else {
-    paramVals[name] = paramVal;
-  }
-
-  paramMetas[name] = paramInfo;
-
+    control.onFinishChange(function(val) {
+        paramMetas[name].osc.args[0] = val;
+        socket.send(JSON.stringify(paramMetas[name].osc));
+    });
 
 }
 
 function receviedParam(data) {}
 
 /* Possible osc addresses: (see ofxRemoteUI.h)
-    HELO    –   In Response to client HELO
-    REQU    –   'REQU OK' indicates end of requested param lists
-    SEND    –   Followed by a param
-    PREL    –   Preset name list
-    SETP    -   Followed by OK, server set preset ack
-    MISP    -   Missing presets
-    SAVP    –   Save current params as preset
-    DELP    -   Delete a preset
-    RESX    -   Reset to default XML values
-    RESD    –   Reset to code defaults (pre-RUI invocation)
-    SAVp    -   Save a preset, different from SAVP ? ? ?
-    DELp    -   Delete a preset, diffent from DELP ? ? ?
-    TEST    -   Part of ping-pong keep alive exchange
-    CIAO    -   Signal disconnect
+HELO    –   In Response to client HELO
+REQU    –   'REQU OK' indicates end of requested param lists
+SEND    –   Followed by a param
+PREL    –   Preset name list
+SETP    -   Followed by OK, server set preset ack
+MISP    -   Missing presets
+SAVP    –   Save current params as preset
+DELP    -   Delete a preset
+RESX    -   Reset to default XML values
+RESD    –   Reset to code defaults (pre-RUI invocation)
+SAVp    -   Save a preset, different from SAVP ? ? ?
+DELp    -   Delete a preset, diffent from DELP ? ? ?
+TEST    -   Part of ping-pong keep alive exchange
+CIAO    -   Signal disconnect
 */
 
 // Server sends HELO after we say HELO, opening connection
 // Next we want to request the param list
 function gotHELO(osc) {
-  sendOSC("REQU");
+    setInterval(function() {sendOSC("TEST")}, 500);
+    sendOSC("REQU");
 }
 
 // Should receive a message like { addr : "/REQU OK"} to signal end of params
 function gotREQU(osc) {
-  var headerPieces = msg.addr.split(' ');
-  if (headerPieces.len == 2 || headerPieces[1] == "OK") {
-      // TODO Great, we got all the params
-  }
-  else {
-      // UH-OH
-  }
+    var headerPieces = osc.addr.split(' ');
+    if (headerPieces.length == 2 || headerPieces[1] == "OK") {
+        // TODO Great, we got all the params
+    }
+    else {
+        // UH-OH
+    }
 }
 
 // Got TEST, keep alive
 function gotTEST(osc) {
-  // we'll send our own TEST separately
+    // we'll send our own TEST separately
 }
 
 function gotSEND(osc) {
-  var headerPieces = getHeaderPieces(osc);
-  var type = headerPieces[1];
-  if (type == "SPA") { // Its a new group
-    var newGroup = new gui.addFolder(headerPieces[2]);
-    // TODO Colors
-    groups.unshift(newGroup);
-  }
-  else {
-    gotParamViaOsc(osc)
-  }
+    var headerPieces = getHeaderPieces(osc);
+    var type = headerPieces[1];
+    if (type == "SPA") { // Its a new group
+        var newGroup = gui.addFolder(headerPieces[2]);
+        newGroup.open();
+        // TODO Colors
+        groups.unshift(newGroup);
+    }
+    else {
+        setLocalParamViaOsc(osc, type, headerPieces[2]);
+    }
 }
 
-var gotMsg = {
-  "HELO" : gotHELO,
-  "REQU" : gotREQU,
-  "SEND" : gotSEND,
-/*  "PREL" : gotPREL,
-  "SETP" : gotSETP,
-  "MISP" : gotMISP,
-  "SAVP" : gotSAVP,
-  "DELP" : gotDELP,
-  "RESX" : gotRESX,
-  "RESD" : gotRESD,
-  "SAVp" : gotSAVp,
-  "DELp" : gotDELp,
-  "TEST" : gotTEST,
-  "CIAO" : gotCIAO */
+var msgcFuncs = {
+    "HELO" : gotHELO,
+    "REQU" : gotREQU,
+    "SEND" : gotSEND,
+    /*"PREL" : gotPREL,
+    "SETP" : gotSETP,
+    "MISP" : gotMISP,
+    "SAVP" : gotSAVP,
+    "DELP" : gotDELP,
+    "RESX" : gotRESX,
+    "RESD" : gotRESD,
+    "SAVp" : gotSAVp,
+    "DELp" : gotDELp,*/
+    "TEST" : gotTEST,
+    // "CIAO" : gotCIAO
 }
-
-///////////////////////////////////////////////////
-//                   Dat.GUI                     //
-///////////////////////////////////////////////////
-
-var gui = new dat.GUI({ autoPlace: false });
-
-var customContainer = document.getElementById('controls');
-customContainer.appendChild(gui.domElement);
